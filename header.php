b8a48c12ebf8611af79c5e6be44ee6c12bfa69c7
GitPit <rifflifter@gmail.com>
2024-01-10 18:02:46 -0500

    header.php <head> <meta> <body>

---
+++ header.php
@@ -0,0 +1,14 @@
+<?php
+/** */
+?><!doctype html>
+
+<html>
+
+    <head>
+        <meta charset="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+</body>
+
+</html>
