f9f3ef7e7407ca37365069962829fdc70044e72c
GitPit <rifflifter@gmail.com>
2024-01-10 18:11:36 -0500

    <?php bloginfo('charset')?>

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
 <html <?php language_attributes(); ?> >
 
     <head>
-        <meta charset="">
+        <meta charset="<?php bloginfo( 'charset') ?>">
         <meta name="viewport" content="width=device-width, initial-scale=1">
     </head>
     <body>
