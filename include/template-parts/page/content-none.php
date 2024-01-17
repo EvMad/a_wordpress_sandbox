32af1693f395b3a6172b8f18a2a3868b35222f07
GitPit <rifflifter@gmail.com>
2024-01-17 15:26:25 -0500

    added if else for not found

--- include/template-parts/page/content-none.php
+++ include/template-parts/page/content-none.php
@@ -8,4 +8,24 @@
     <h1 class="page-title"><?php esc_html_e( 'Nothing Found', 'Sandbox' ) ?></h1>
 </header>
 
+<div class="page-content">
+    <?php 
+    if ( is_home() && current_user_can( 'publish_posts' ) ) :
+
+        printif(
+            '<p>' . wp_kses(
+                __( 'Ready to publish your first post? <a href="%1$s"> Get Started Here </a>', 'Sandbox'),
+                array(
+                    'a' => array(
+                        'href' => array(),
+                    ),
+                )
+            ) . '</p>',
+            esc_url( admin_url('post-new.php' ) )
+                );
+
+                elseif ( is_search() ) :
+    ?>
+</div>
+
 </section>
