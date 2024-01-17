9db6c3d1af857366cc9da3c5d9ff6e6f67da725b
GitPit <rifflifter@gmail.com>
2024-01-17 15:20:27 -0500

    added content-none.php with <header>

---
+++ include/template-parts/page/content-none.php
@@ -0,0 +1,11 @@
+<?php 
+//template for not found
+?>
+
+<section class="no-results not-found">
+
+<header class="page-header">
+    <h1 class="page-title"><?php esc_html_e( 'Nothing Found', 'Sandbox' ) ?></h1>
+</header>
+
+</section>
