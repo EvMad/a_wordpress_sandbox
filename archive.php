8818e3033b8887ea3e3f07857f08e0705ce15c12
GitPit <rifflifter@gmail.com>
2024-01-17 14:59:24 -0500

    the_archive_title( '<h1 class=archive-title>', '</h1>')

--- archive.php
+++ archive.php
@@ -8,7 +8,9 @@ get_header();
     <main id="main" class="site-main">
         <?php if ( have_posts() ) : ?>
             <header class="archive-page-header">
-                
+                <?php 
+                the_archive_title( '<h1 class="archive-title">', '</h1>')
+                ?>
             </header>
             <?php endif; ?>
     </main>
