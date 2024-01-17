f5c8b40083564df467e813a45ada0c354c3a73e1
GitPit <rifflifter@gmail.com>
2024-01-17 14:57:42 -0500

    if have_posts() <header>

--- archive.php
+++ archive.php
@@ -6,7 +6,11 @@ get_header();
 
 <div id="primary" class="content-area">
     <main id="main" class="site-main">
-        <?php ?>
+        <?php if ( have_posts() ) : ?>
+            <header class="archive-page-header">
+                
+            </header>
+            <?php endif; ?>
     </main>
 </div>
 
