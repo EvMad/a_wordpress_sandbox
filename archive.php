7b27e91006177d803dc040f733729e8e0e2affd2
GitPit <rifflifter@gmail.com>
2024-01-17 14:55:07 -0500

    archive.php basic wrappers

---
+++ archive.php
@@ -0,0 +1,15 @@
+<?php
+//archive template
+
+get_header();
+?>
+
+<div id="primary" class="content-area">
+    <main id="main" class="site-main">
+        <?php ?>
+    </main>
+</div>
+
+<?php 
+get_footer();
+?>
