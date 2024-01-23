a1d389a470ea528e0c1393497f6e90cf1a6b30aa
GitPit <rifflifter@gmail.com>
2024-01-23 15:02:46 -0500

    added excerpt div

--- include/template-parts/page/content-search.php
+++ include/template-parts/page/content-search.php
@@ -5,5 +5,9 @@
     <?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>') ?>
 </header>
 
+<div class="entry-summary">
+<?php the_excerpt(); ?>
+</div>
+
 
 </article>
