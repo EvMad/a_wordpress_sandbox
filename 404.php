f879eacf58e34538e954286940cbd0ec02a2ba66
GitPit <rifflifter@gmail.com>
2024-02-02 13:12:23 -0500

    added 404 content and search form

--- 404.php
+++ 404.php
@@ -10,6 +10,15 @@
         <header class="page-header">
             <h1 class="page-title"><?php esc_html_e( 'Oops! That page cannot be found.', 'Sandbox' ); ?></h1>
         </header>
+
+    <div class="page-content">
+
+        <p><?php esc_html_e( 'It looks like nothing was found at this location. Try the links below or the search form.' ); ?></p>
+
+        <?php get_search_form(); ?>
+
+    </div>
+
     </section>
 
 </main>
