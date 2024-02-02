f243dff38a1d666ead32ca9ecd89b91d8c4483af
GitPit <rifflifter@gmail.com>
2024-02-02 13:06:49 -0500

    added main and sectio wrapped divs to 404

--- 404.php
+++ 404.php
@@ -2,3 +2,14 @@
     @get_header();
 ?>
 
+<div id="primary" class="content-area">
+
+<main id="main" class="site-main">
+
+    <section claass="error-404 not-found">
+
+    </section>
+
+</main>
+
+</div>
