77ae4a651aa43c611bfe95f3d3cd5e0259a6d35a
GitPit <rifflifter@gmail.com>
2024-02-02 13:23:12 -0500

    if statement to show comment

--- comments.php
+++ comments.php
@@ -9,7 +9,14 @@ if ( post_password_required() ) {
 <div id="comments" class="comments-area">
 <?php if ( have_commets() ) : ?>
     <h2 class="comments-title">
-        
+        <?php
+        $comment_count = get_comments_number();
+        if ( '1' === $comment_count ) {
+            printf(
+                esc_html__( 'Comments (1)', 'Sandbox' )
+            );
+        }
+        ?>
     </h2>
 <?php endif; ?>
 </div>
