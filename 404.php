353e63b2fe916f5a03ebb30129011cc5529b7b77
GitPit <rifflifter@gmail.com>
2024-02-02 13:09:35 -0500

    <h1 class=page-title><?php esc_html_e( 'Oops! That page cannot be found.',

--- 404.php
+++ 404.php
@@ -7,7 +7,9 @@
 <main id="main" class="site-main">
 
     <section class="error-404 not-found">
-
+        <header class="page-header">
+            <h1 class="page-title"><?php esc_html_e( 'Oops! That page cannot be found.', 'Sandbox' ); ?></h1>
+        </header>
     </section>
 
 </main>
