174cf2a83040775455f4978157913dd4d7f323b4
GitPit <rifflifter@gmail.com>
2024-02-02 13:19:49 -0500

    wrapper div for comments

--- comments.php
+++ comments.php
@@ -6,3 +6,10 @@ if ( post_password_required() ) {
 
 ?>
 
+<div id="comments" class="comments-area">
+<?php if ( have_commets() ) : ?>
+    <h2 class="comments-title">
+        
+    </h2>
+<?php endif; ?>
+</div>
