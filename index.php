b74332963bf74cb0a7861b1ee82add255226dc06
GitPit <rifflifter@gmail.com>
2024-02-16 14:42:01 -0500

    updated index.php wrappers

--- index.php
+++ index.php
@@ -1,21 +1,32 @@
 <?php
 /** */
+?>
 
+<div id="primary" class="content-area">
+    <main id="main" class="site-main">
 
-get_header();
+<?php 
+    get_header();
 
     get_search_form();
-
+    
     if ( have_posts() ) :
-
+    
         while ( have_posts() ) :
-
+    
             the_post();
             get_template_part( 'template-parts/post/content' );
         endwhile;
-
+    
     endif;
-
-get_footer();
-
+    
+    get_footer();
 ?>
+
+
+    </main>
+</div>
+
+
+
+
