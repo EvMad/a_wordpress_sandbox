6abbf871c4696f58982f74999248c37e5d44de09
GitPit <rifflifter@gmail.com>
2024-02-16 14:44:35 -0500

    moved get_header() to line 2

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
-/** */
+get_header();
 ?>
 
 <div id="primary" class="content-area">
     <main id="main" class="site-main">
 
 <?php 
-    get_header();
+    
     
     if ( have_posts() ) :
     
