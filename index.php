9a2df56923eeb7d5de7ce9dbbd8daa5caa290ddc
GitPit <rifflifter@gmail.com>
2024-02-22 16:24:33 -0500

    added pagination to index.php

--- index.php
+++ index.php
@@ -15,6 +15,16 @@ get_header();
             the_post();
             get_template_part( 'template-parts/post/content' );
         endwhile;
+
+
+        echo paginate_links( [
+            'prev_text' => esc_html__( 'Prev', 'Sandbox'),
+            'next_text' => esc_html__( 'Next', 'Sandbox'),
+        ] );
+
+        else
+
+        get_template_part( 'template-parts/page/content', 'none' )
     
     endif;
     
