c3365c2d3d565ddcf6d26fc53aa90e57e63554fd
GitPit <rifflifter@gmail.com>
2024-02-22 16:43:13 -0500

    get_post_format() line 15

--- single.php
+++ single.php
@@ -12,7 +12,7 @@ get_header();
 while ( have_posts() ) :
     the_post();
 
-    get_template_part( 'template-parts/post/content' );
+    get_template_part( 'template-parts/post/content', get_post_format() );
 endwhile;
 
 // comments template
